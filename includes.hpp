58e13024213f493d98f4090004f8ebedd272951a
Amir Setzemsky <5632815@gmail.com>
2022-11-08 12:22:41 +0200

    smaller 'mob' file

--- includes.hpp
+++ includes.hpp
@@ -7,6 +7,8 @@
 #include <cassert>
 #include <vector>
 #include <set>
+#include <chrono>
+#include <thread>
 
 #define OLC_PGE_APPLICATION
 #include <olcPixelGameEngine.h>
@@ -21,13 +23,19 @@
 #include "map.hpp"
 
 #define RESOLUTION 4
+#define MAX_FPS 60.0f
+
 #define BLOCK_SIZE 32
 #define fBLOCK_SIZE ((float)BLOCK_SIZE)
 #define MAP_DIR std::string("maps/")
 
-#define MOB_SPRITE_WIDTH  180
-#define MOB_SPRITE_HEIGHT 20
+#define MOB_SPRITE_WIDTH  (16 * 9)
+#define MOB_SPRITE_HEIGHT 16
 #define MOB_SIZE olc::vi2d{MOB_SPRITE_HEIGHT, MOB_SPRITE_HEIGHT}
+
+#define TARGET_FILE_SIZE olc::vi2d{13,13}
+#define fTARGET_FILE_SIZE olc::vf2d{TARGET_FILE_SIZE}
+#define TARGET_SIZE (fBLOCK_SIZE / fTARGET_FILE_SIZE) 
 
 #define BG_COLOR olc::CYAN
 // World Types
