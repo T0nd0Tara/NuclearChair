2760614a7742e2a8b8669a913046cf7f62158e40
Amir Setzemsky <5632815@gmail.com>
2022-11-09 20:54:27 +0200

    move to c++20

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 g++ -I ~/code-libs/olcPixelGameEngine \
     -I ~/code-libs/magic_enum/include \
     -o main main.cpp \
-    -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17 -ggdb \
+    -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++20 -ggdb \
     -Wall -Wextra -Wno-unknown-pragmas -Wno-unused-parameter -Wno-sign-compare -Wno-unused-but-set-variable \
     && ./main
